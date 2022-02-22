d7a4abde2f25564da1c3c4f959b3f163a5957492
MGPasinetti <mg.pasinetti@gmail.com>
2022-02-22 16:19:55 +0100

    testing

---
+++ main.js
@@ -0,0 +1,14 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// Ricordate di scrivere solo il codice JavaScript e non modificare HTML e CSS
+// Consiglio del giorno:
+// Partite dal comprendere bene come è fatto il DOM e il CSS, potete usarli a vostro vantaggio!
+// P.S.: l'url del logo che trovate nell'html non è più raggiungibile, e sul sito di Boolean adesso trovate un svg per il logo. Vedete un po' voi se volete risolvere il problema ma non è fondamentale che funzioni.
+
+
+const element = document.querySelector(`.header-right a`)
+
+// $("button").click(function(){
+//     $("p").toggle();
+// });
